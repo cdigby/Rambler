4237a7a38f99ffc76de4c18eb8bdadceb4f4c5a9
Chris Digby <christopherdigby@gmail.com>
2021-03-18 17:38:19 +0000

    Rename to Rambler internally

--- Rambler/wsgi.py
+++ Rambler/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for WalkingApp project.
+WSGI config for Rambler project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WalkingApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Rambler.settings')
 
 application = get_wsgi_application()
