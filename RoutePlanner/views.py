4a37f76c705836b051b3350df19ae98531691810
MikeFoley22 <80759304+MikeFoley22@users.noreply.github.com>
2021-03-25 17:25:04 +0000

    Front end now talks to back end, back end creates route model when provided with POST request

--- RoutePlanner/views.py
+++ RoutePlanner/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 from . import models
 
 
@@ -7,9 +9,13 @@ def routePlanner(request):
     if request.method == 'GET':
         return render(request, 'planner/planner.html')
     elif request.method == 'POST':
-        title = request.route.title
-        points = request.route.points
-        route = models.Route.objects.create_route(title, points)
+        jsonData = json.loads(request.body.decode())
+        title = (jsonData['title'])
+        points = (jsonData['points'])
+        route = models.Route()
+        route.create_route(title, points)
+        route.save()
+        return HttpResponse("OK")
 
 
 
