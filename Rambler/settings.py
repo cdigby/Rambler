69d580795278ea8480aa55c81b97ea9228be2475
Chris Digby <christopherdigby@gmail.com>
2021-04-08 14:39:38 +0100

    Remove crispy-forms

--- Rambler/settings.py
+++ Rambler/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
     'users.apps.UsersConfig',
     'home.apps.HomeConfig'
 ]
