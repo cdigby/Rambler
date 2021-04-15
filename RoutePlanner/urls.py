185ec71010c520ab1c5aacb8564b92a00d290853
MikeFoley22 <80759304+MikeFoley22@users.noreply.github.com>
2021-04-15 16:07:56 +0100

    Minor changes

--- RoutePlanner/urls.py
+++ RoutePlanner/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('planner/', views.routePlanner, name='route-planner'),
+    path('test/', views.getRoutes, name='test'),
 ]
--- RoutePlanner/views.py
+++ RoutePlanner/views.py
@@ -18,6 +18,35 @@ def routePlanner(request):
         route.save()
         print(models.Route.objects.get(pk=1).title)
         return HttpResponse("OK")
+"""
+def feed(request):
+    if request.method == 'GET':
+        return render(request, 'addresss')
+    elif request.method == 'POST':
+        jsonData = json.loads(request.body.decode())
+        operation = (jsonData['operation'])
+        routeNo = (jsonData['routeNumber'])
+        if (operation == "like"):
+            #get model
+            #change rating
+            #save model
+        elif (operation == "dislike"):
+            # get model
+            # change rating
+            # save model
+        return HttpResponse("OK")
+"""
+def getRoutes(request):
+    Routes =[]
+    num_of_routes = models.Route.objects.all().count()
+    print(num_of_routes)
+    for i in range (num_of_routes):
+        print(models.Route.objects.get(pk=1).title)
 
+        #Routes.append({
 
+         #   'title': models.Route.objects.get(pk=1).title
+
+        #})
+    return HttpResponse("OK")
 
