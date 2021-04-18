79cb3c8d90183456b874c4cbf71eed11aee76a63
MikeFoley22 <80759304+MikeFoley22@users.noreply.github.com>
2021-04-18 12:16:03 +0100

    Building XHR function

--- Feed/views.py
+++ Feed/views.py
@@ -7,18 +7,26 @@ from django.http import HttpResponse
 
 # Create your views here.
 def display_feed(request):
+    if (request.POST):
+        jsonData = json.loads(request.body.decode())
+        function = (jsonData['function'])
+        if (function == "LIKE"):
+            print ("LIKE")
+        elif (function == "DISLIKE"):
+            print ("DISLIKE")
+        return HttpResponse("OK")
+    else:
+        print("########")
 
-    print("########")
+        route = Route.objects.get(pk=1)
 
-    route = Route.objects.get(pk=1)
+        print(route.title)
 
-    print(route.title)
+        route_info = {
+            'title': route.title,
+            'description': route.description,
+            'points': route.points,
+            'rating': route.get_rating(),
+        }
 
-    route_info = {
-        'title' : route.title,
-        'description' : route.description,
-        'points' : route.points,
-        'rating' : route.get_rating(),
-    }
-
-    return render(request, 'Feed/Feed.html', {'route': route_info})
+        return render(request, 'Feed/Feed.html', {'route': route_info})
