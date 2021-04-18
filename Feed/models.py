1fd6c0e6b58aaee31e757edd639b9b330df9dcee
MikeFoley22 <80759304+MikeFoley22@users.noreply.github.com>
2021-04-18 11:34:13 +0100

    Models created

--- Feed/models.py
+++ Feed/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Route(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    points = models.TextField()
+
+    def get_likes(self):
+        return len(Like.objects.get(pk=self.id).all)
+
+    def create_route(self, title, desc, points):
+        self.title = title
+        self.description = desc
+        self.points = points
+
+    def save(self, *args, **kwargs):
+        super(Route, self).save(*args, **kwargs)
+        print("Saved route: ", self.title, ", Desc: ", self.description, " with points: ", self.points)
+
+class Like(models.Model):
+    user = models.IntegerField()
+    route = models.IntegerField()
+
+class Dislike(models.Model):
+    user = models.IntegerField()
+    route = models.IntegerField()
--- Rambler/settings.py
+++ Rambler/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
-    'home.apps.HomeConfig'
+    'home.apps.HomeConfig',
+    'Feed',
 ]
 
 MIDDLEWARE = [
