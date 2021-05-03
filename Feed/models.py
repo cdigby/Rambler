3a9276ca2333923fc5a2e0b419adae42c25d9668
Chris Digby <christopherdigby@gmail.com>
2021-05-03 15:55:08 +0100

    Model being displayed in template

--- Feed/models.py
+++ Feed/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Route(models.Model):
     title = models.CharField(max_length=50)
@@ -7,7 +8,20 @@ class Route(models.Model):
     points = models.TextField()
 
     def get_likes(self):
-        return len(Like.objects.get(pk=self.id).all)
+        try:
+            return len(Like.objects.get(pk=self.id).all)
+        except models.ObjectDoesNotExist:
+            return 0;
+
+    def get_dislikes(self):
+        try:
+            return len(Dislike.objects.get(pk=self.id).all)
+        except models.ObjectDoesNotExist:
+            return 0;
+
+    def get_rating(self):
+
+        return self.get_likes() - self.get_dislikes()
 
     def create_route(self, title, desc, points):
         self.title = title
@@ -18,9 +32,11 @@ class Route(models.Model):
         super(Route, self).save(*args, **kwargs)
         print("Saved route: ", self.title, ", Desc: ", self.description, " with points: ", self.points)
 
+
 class Like(models.Model):
     user = models.IntegerField()
     route = models.IntegerField()
+
 
 class Dislike(models.Model):
     user = models.IntegerField()
--- Feed/views.py
+++ Feed/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Route
+from django.shortcuts import get_object_or_404
+
+import json
+from django.http import HttpResponse
 
 # Create your views here.
+def display_feed(request):
+
+    print("########")
+
+    route = Route.objects.get(pk=1)
+
+    print(route.title)
+
+    route_info = {
+        'title' : route.title,
+        'description' : route.description,
+        'points' : route.points,
+        'rating' : route.get_rating(),
+    }
+
+    return render(request, 'Feed/Feed.html', {'route': route_info})
