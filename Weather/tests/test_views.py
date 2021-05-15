94ebea33d6835fc77cbc651f7b12f3839eed7260
Dan <DanJezeph@outlook.com>
2021-05-15 16:14:18 +0100

    Testing files

---
+++ Weather/tests/test_views.py
@@ -0,0 +1,15 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+import json
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.weather_url = reverse('weather-page')
+
+    def test_page(self):
+        response = self.client.get(self.weather_url)
+        
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'weather/weatherPage.html')
