ad35c0f86b0c658b6523e2bd0a87128e6473ae81
MikeFoley22 <80759304+MikeFoley22@users.noreply.github.com>
2021-05-16 10:54:07 +0100

    Routing working, route pages up and running

--- Rambler/settings.py
+++ Rambler/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'home.apps.HomeConfig',
     'Feed',
+    'RoutePages',
 ]
 
 MIDDLEWARE = [
--- RoutePages/views.py
+++ RoutePages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from RoutePlanner import models
 
-# Create your views here.
+def displayRoutePage(request, id):
+    route = models.Route.objects.get(pk=id)
+    routeDict = {
+        'title': route.title,
+        'description': route.description,
+        'points': route.points,
+        'length': route.length,
+        'image': route.image,
+    }
+    route.printRoute()
+    return render(request, 'routePage.html', {'route': routeDict})
+
