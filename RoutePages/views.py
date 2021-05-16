33a6eafa371670ba578023ea51c2eb9809071d4f
MikeFoley22 <80759304+MikeFoley22@users.noreply.github.com>
2021-05-16 11:13:16 +0100

    Styled route pages

--- RoutePages/views.py
+++ RoutePages/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from RoutePlanner import models
 
@@ -7,8 +8,13 @@ def displayRoutePage(request, id):
         'title': route.title,
         'description': route.description,
         'points': route.points,
-        'length': route.length,
+        'length': round(route.length, 2),
         'image': route.image,
+        'rating': route.rating,
+        'id': route.id,
+        'tags': route.tags,
+        'user': User.objects.get(pk=route.user).username,
+        'date': route.date,
     }
     route.printRoute()
     return render(request, 'routePage.html', {'route': routeDict})
