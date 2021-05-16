eb88f1416e1c7e391e764e0d347eb59645ecbacf
MikeFoley22 <80759304+MikeFoley22@users.noreply.github.com>
2021-05-16 16:32:29 +0100

    Added user, tags etc

--- RoutePages/views.py
+++ RoutePages/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from RoutePlanner import models
 
@@ -9,6 +10,11 @@ def displayRoutePage(request, id):
         'points': route.points,
         'length': round(route.length, 2),
         'image': route.image,
+        'rating': route.rating,
+        'id': route.id,
+        'tags': route.tags,
+        'user': User.objects.get(pk=route.user).username,
+        'date': route.date,
     }
     route.printRoute()
     return render(request, 'routePage.html', {'route': routeDict})
