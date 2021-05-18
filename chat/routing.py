c19557a2b60aa68458d2b54e574fe841fc97f05c
JD1517 <joshua.g.davies@outlook.com>
2021-05-18 10:59:41 +0100

    Revert "Changes to room.html and index.html"
    This reverts commit 2a17faedccdf050dfe688a8eba7dc173019be31d.

--- chat/routing.py
+++ chat/routing.py
@@ -1,8 +1,7 @@
 from django.urls import re_path
-from django.urls import path
+
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/<str:room_name>/', consumers.ChatRoomConsumer.as_asgi()),
-  #  re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatRoomConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatRoomConsumer.as_asgi()),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-from .models import Message
 
 def index(request):
     return render(request, 'index.html', {})
 
 def room(request, room_name):
-        username = request.GET.get('username', 'anonymous_user')
-        messages = Message.objects.filter(room=room_name)[0:15]
-
-        return render (request, 'room.html', {'room_name': room_name, 'username': username, 'messages': messages})
+    return render(request, 'room.html', {
+        'room_name': room_name
+    })
